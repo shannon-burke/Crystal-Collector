b2d19e96ca670b122f5eadf2943fa3b8b30ee5d6
Shannon <shannonburke.art@gmail.com>
2019-06-23 23:08:25 -0500

    win functionality working

--- assets/game.js
+++ assets/game.js
@@ -10,26 +10,64 @@ $("#wins").text("Succesful days: " + wins);
 var losses = 0;
 $("#losses").text("Ripped bags: " + losses);
 
+function winFunction() {
+
+};
+
 $("#crystal1").on("click", function () {
     currentScore += 1;
     console.log(currentScore);
     $("#current-score").text(currentScore);
+
+    if (currentScore === targetNumber) {
+        currentScore = 0;
+        targetNumber = Math.floor(Math.random() * 101 + 19);
+        wins += 1;
+        $("#current-score").text(currentScore);
+        $("#number-to-guess").text(targetNumber);
+        $("#wins").text("Succesful days: " + wins);
+    };
 });
 
 $("#crystal2").on("click", function () {
     currentScore += 2;
     console.log(currentScore);
     $("#current-score").text(currentScore);
+
+    if (currentScore === targetNumber) {
+        currentScore = 0;
+        targetNumber = Math.floor(Math.random() * 101 + 19);
+        wins += 1;
+        $("#wins").text("Succesful days: " + wins);
+    };
 });
 
 $("#crystal3").on("click", function () {
     currentScore += 3;
     console.log(currentScore);
     $("#current-score").text(currentScore);
+
+    if (currentScore === targetNumber) {
+        currentScore = 0;
+        targetNumber = Math.floor(Math.random() * 101 + 19);
+        wins += 1;
+        $("#wins").text("Succesful days: " + wins);
+    };
 });
 
 $("#crystal4").on("click", function () {
-    currentScore += 4;
+    currentScore += 10;
     console.log(currentScore);
     $("#current-score").text(currentScore);
+
+    if (currentScore === targetNumber) {
+        currentScore = 0;
+        targetNumber = Math.floor(Math.random() * 101 + 19);
+        wins += 1;
+        $("#wins").text("Succesful days: " + wins);
+    };
+
+
 });
+
+
